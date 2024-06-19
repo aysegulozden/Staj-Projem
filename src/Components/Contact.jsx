5958e3c51a6567e138805d36d05819d9e123bc34
Aysegul Ozden <ozdenaysegul728@gmail.com>
2024-06-19 23:29:47 +0300

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import styles from '../CSS/Contact.module.css'
 import EmailIcon from '@mui/icons-material/Email';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+
 
 function Contact() {
+
+
     return (
         <div className={styles.container}>
-            <div className={styles.dsc}>
-                <h2>Bizimle İletişime Geçin
-                    <EmailIcon className={styles.emailicon} />
+            <div className={styles.contact}>
+                <h2>
+                    <EmailIcon className={styles.emailicon} />Bizimle İletişime Geçin
+
                 </h2>
 
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, quae esse facere accusantium accusamus distinctio odit, ratione vero, dolores atque mollitia? Reprehenderit corrupti corporis placeat! Labore nam obcaecati quia assumenda praesentium quaerat, repellat aspernatur ab ex saepe nisi temporibus rerum consequuntur porro! Hic repellat saepe exercitationem possimus quibusdam eos laudantium?</p>
-                <ul>
+                <ul className={styles.list}>
                     <li>0222 111 33 44 </li>
                     <li>iletisim@gmail.com</li>
                     <li>@socail_people</li>
                 </ul>
 
             </div>
-            <div className={styles.from}>
-                <h2>Bilgileriniz</h2>
+            <div className={styles.contact}>
+                <form >
+                    <label>Adınız</label>
+                    <input type="text" name='adını' placeholder='Adınız' required />
+                    <label>Telefon Numaranız</label>
+                    <input type="text" name="telefon" placeholder='Telefon Numaranız' required />
+                    <label>E-Mail</label>
+                    <input type="email" name="email" placeholder='E-Mail' required />
+                    <label > Mesajınızı Yazınız</label>
+                    <textarea name="mesaj" rows={10} placeholder='Mesajınız' required ></textarea>
+                    <button type='submit' className={styles.btn} >Gönder</button>
+                </form>
+
             </div>
         </div>
     )
